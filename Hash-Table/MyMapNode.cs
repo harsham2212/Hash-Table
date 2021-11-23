ddd1014956180966297f9eb314bc16024bd59cb4
Harsh Singh <kmrharsh50@gmail.com>
2021-11-23 16:03:00 +0530

    [Harsh]Added:frequency of a word in short given sentence Hash-Table Implemented

---
+++ Hash-Table/MyMapNode.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class MyMapNode<K,V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedList = GetLinkedList(position);
+            foreach(KeyValue<K,V> items in LinkedList)
+                {
+                if(items.Key.Equals(key))
+                {
+                    return items.value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key,V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, value = value };
+            LinkedList.AddLast(item);
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach(KeyValue<K,V> item in LinkedList)
+            {
+                if(item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                LinkedList.Remove(foundItem);
+            }
+        }
+        protected LinkedList<KeyValue<K,V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> LinkedList = items[position];
+            if(LinkedList == null)
+            {
+                LinkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = LinkedList;
+            }
+            return LinkedList;
+        }
+    }
+    public struct KeyValue<k,V>
+    {
+        public k Key { get; set; }
+        public V value { get; set; }
+    }
+}
---
+++ Hash-Table/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash5 = hash.Get("3");
+            Console.WriteLine("3th index Value:" + hash5);
+            string hash2 = hash.Get("5");
+            Console.WriteLine("5th index Value:" + hash2);
+        }
+    }
+}
