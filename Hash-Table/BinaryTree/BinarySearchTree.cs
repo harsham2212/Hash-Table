b54facecc91696b4fb6e6c92cde0f3737320cd4a
Harsh Singh <kmrharsh50@gmail.com>
2021-11-24 22:11:03 +0530

    [Harsh]Added:Creating Binary Tree Hash-Table feature implemented

---
+++ Hash-Table/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable.BinarySearchTree
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;  
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if (currentNodeValue.CompareTo(item) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BinarySearchTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+        }
+
+        public bool IfExists(T element, BinarySearchTree<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the Element in Binary Search Tree" + " " + node.NodeData);
+                result = true;
+            }
+            else
+            {
+                Console.WriteLine("Current Element is {0} in Binary Search Tree", node.NodeData);
+            }
+            if (element.CompareTo(node.NodeData) < 0)
+            {
+                IfExists(element, node.LeftTree);
+            }
+            if (element.CompareTo(node.NodeData) > 0)
+            {
+                IfExists(element, node.RightTree);
+            }
+            return result;
+        }
+    }
+}
--- Hash-Table/Program.cs
+++ Hash-Table/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HashTable.BinarySearchTree;
+using System;
 
 namespace HashTable
 {
@@ -6,20 +7,12 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hash Table");
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
-
-            string hash5 = hash.Get("3");
-            Console.WriteLine("3th index Value:" + hash5);
-            string hash2 = hash.Get("5");
-            Console.WriteLine("5th index Value:" + hash2);
-            hash.Remove("2");
+            Console.WriteLine("Binary Search Tree Demo");
+            BinarySearchTree<int> Binary = new BinarySearchTree<int>(56);
+            Binary.Insert(30);
+            Binary.Insert(70);
+            Binary.Display();
+            Binary.GetSize();
         }
     }
 }
